41f828fa6da778f4cf1693289f6681d0d4b6823c
Adam <adam@messagecloud.com>
2018-08-31 12:39:55 +0100

    image upload and trending words finished.

--- includes/form_handlers/delete_post.php
+++ includes/form_handlers/delete_post.php
@@ -7,6 +7,11 @@ require '../../config/config.php';
 	if(isset($_POST['result'])) {
 		if($_POST['result'] == 'true')
 			$query = mysqli_query($con, "UPDATE posts SET deleted='yes' WHERE id='$post_id'");
+		    $user_query = mysqli_query($con, "SELECT added_by FROM posts WHERE id='$post_id'");
+		    $row = mysqli_fetch_array($user_query);
+		    $user_to_decrement = $row['added_by'];
+
+		    $decrement_posts_query = mysqli_query($con, "UPDATE users SET num_posts=num_posts-1 WHERE username='$user_to_decrement'");
 	}
 
 ?>
